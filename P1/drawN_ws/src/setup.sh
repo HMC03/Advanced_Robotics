bf5c3c022a78a32c28f3913512a6610a29cb73d4
hayden <haydenmcameron@proton.me>
2025-09-06 01:51:54 -0400

    cleans up drawN_ws

--- P1/drawN_ws/src/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# TurtleBot3 environment setup script
-
-# Source ROS Jazzy installation
-source /opt/ros/jazzy/setup.bash
-
-# Source local workspace
-source ./install/local_setup.bash
